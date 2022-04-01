777fc1804a9a8dac855782d9240f44edb4ae9e2d
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-04-01 17:22:25 -0500

    creando aplicación
    creando aplicación

---
+++ src/main/java/co/edu/udea/gestionpersonalservicios/GestionPersonalServiciosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.gestionpersonalservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonalServiciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPersonalServiciosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/udea/gestionpersonalservicios/GestionPersonalServiciosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.gestionpersonalservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonalServiciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
