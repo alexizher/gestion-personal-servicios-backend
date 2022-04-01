c9f174462e73a5d161ddba98ed8901134f418287
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-04-01 17:23:25 -0500

    Repository
    Se crea el repositorio

---
+++ src/main/java/co/edu/udea/gestionpersonalservicios/repository/SolicitudRepository.java
@@ -0,0 +1,13 @@
+package co.edu.udea.gestionpersonalservicios.repository;
+
+import co.edu.udea.gestionpersonalservicios.entity.SolicitudEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RepositoryRestResource(path = "solicitudes", collectionResourceRel = "solicitudes")
+public interface SolicitudRepository extends JpaRepository<SolicitudEntity, Integer> {
+
+
+}
