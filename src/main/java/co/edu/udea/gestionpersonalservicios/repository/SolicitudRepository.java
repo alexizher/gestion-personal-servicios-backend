2d145eac47f4312a89232c77315d75b464093474
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-04-03 20:42:29 -0500

    Update SolicitudRepository.java
    se agrega la anotación @CrossOrigin para permitir la consulta desde el front de angular

--- src/main/java/co/edu/udea/gestionpersonalservicios/repository/SolicitudRepository.java
+++ src/main/java/co/edu/udea/gestionpersonalservicios/repository/SolicitudRepository.java
@@ -4,7 +4,9 @@ import co.edu.udea.gestionpersonalservicios.entity.SolicitudEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Repository
 @RepositoryRestResource(path = "solicitudes", collectionResourceRel = "solicitudes")
 public interface SolicitudRepository extends JpaRepository<SolicitudEntity, Integer> {
